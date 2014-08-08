aa900399d74aefd65c653e3cdc335e661d063443
cguanzon <cguanzon@instructure.com>
2014-08-07 23:09:46 -0600

    changed initial code

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,22 @@
-var feeds = [];
-    
-angular.module('feedModule', ['ngResource'])
-    .factory('FeedLoader', function ($resource) {
-        return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-            fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
-        });
-    })
-    .service('FeedList', function ($rootScope, FeedLoader) {
-        this.get = function() {
-            var feedSources = [
-                {title: 'Slashdot', url: 'http://rss.slashdot.org/Slashdot/slashdot'},
-                {title: 'Tweakers', url: 'http://feeds.feedburner.com/tweakers/mixed'},
-                {title: 'Wired', url: 'http://feeds.wired.com/wired/index'},
-            ];
-            if (feeds.length === 0) {
-                for (var i=0; i<feedSources.length; i++) {
-                    FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
-                        var feed = data.responseData.feed;
-                        feeds.push(feed);
-                    });
-                }
-            }
-            return feeds;
-        };
-    })
-    .controller('FeedCtrl', function ($scope, FeedList) {
-        $scope.feeds = FeedList.get();
-        $scope.$on('FeedList', function (event, data) {
-            $scope.feeds = data;
-        });
-    });
+'use strict';
+ 
+var App = angular.module('RSSFeedApp', []);
+
+App.controller("FeedCtrl", ['$scope','FeedService', function ($scope,Feed) {    
+	$scope.loadButonText="Load";
+	$scope.loadFeed=function(e){        
+		Feed.parseFeed($scope.feedSrc).then(function(res){
+			$scope.loadButonText=angular.element(e.target).text();
+			$scope.feeds=res.data.responseData.feed.entries;
+		});
+	}
+}]);
+
+App.factory('FeedService',['$http',function($http){
+	return {
+		parseFeed : function(url){
+			return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+		}
+	}
+}]);
+
